8c237b0722b992d00b7de7b99a96cd8479d2fc46
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-09-27 10:25:11 +0530

    completed image carousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,3 +1,4 @@
+import { MoveLeft, MoveRight } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function ImageCarousel({ images }: { images: string[] }) {
@@ -5,19 +6,47 @@ export default function ImageCarousel({ images }: { images: string[] }) {
   const [ind, setInd] = useState(0);
   const [currentInd, setCurrentInd] = useState(0);
   useEffect(() => {
-    const intervalID = setInterval(() => {
+    const timeoutID = setTimeout(() => {
       setCurrentInd(ind);
       setInd((ind + 1) % images.length);
     }, 2000);
 
     return () => {
-      clearInterval(intervalID);
+      clearTimeout(timeoutID);
     };
   }, [ind, images]);
 
   return (
     <div className="flex justify-center items-center h-[100vh] w-[100vw] border border-white">
+      <MoveLeft
+        className="border border-white bg-inherit rounded-full"
+        cursor={"pointer"}
+        size={50}
+        onClick={() => {
+          console.log(currentInd, ind);
+          setCurrentInd(
+            currentInd === 0
+              ? images.length - 1
+              : (currentInd - 1) % images.length
+          );
+          setInd(
+            currentInd === 0
+              ? images.length - 1
+              : (currentInd - 1) % images.length
+          );
+        }}
+      />
       <img src={images[ind]} key={ind} width={500} height={500} />
+      <MoveRight
+        className="border border-white bg-inherit rounded-full"
+        size={50}
+        cursor={"pointer"}
+        onClick={() => {
+          console.log("Clicked");
+          setCurrentInd((currentInd + 1) % images.length);
+          setInd((currentInd + 1) % images.length);
+        }}
+      />
     </div>
   );
 }
